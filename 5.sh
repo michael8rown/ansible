8451af6a5175e8f1e65a13d6202e721364380a4e
Michael Brown <michael8rown@gmail.com>
2024-04-28 14:45:02 -0500

    more edits

--- 5.sh
+++ 5.sh
@@ -8,14 +8,16 @@ echo "timeout 5" >> /boot/loader/loader.conf
 echo "console-mode keep" >> /boot/loader/loader.conf
 echo "editor no" >> /boot/loader/loader.conf
 
-f=`blkid | grep /dev/vda2 | sed -E 's/^.+ UUID="([^"]+)".+$/\1/g'`
+getroot=`df -h | grep -E '.+/$'`
+rootdev=`echo $getroot | sed -E 's/^([^ ]+) .*$/\1/g'`
+f=`blkid | grep $rootdev | sed -E 's/^.+ UUID="([^"]+)".+$/\1/g'`
 
 echo "title   Arch Linux LTS" >> /boot/loader/entries/arch-lts.conf
 echo "linux   /vmlinuz-linux-lts" >> /boot/loader/entries/arch-lts.conf
 echo "initrd  /initramfs-linux-lts.img" >> /boot/loader/entries/arch-lts.conf
-echo "options root=UUID=$f rw" >> /boot/loader/entries/arch-lts.conf
+echo "options root=UUID=${f} rw" >> /boot/loader/entries/arch-lts.conf
 
 echo "title   Arch Linux LTS (fallback)" >> /boot/loader/entries/arch-lts-fallback.conf
 echo "linux   /vmlinuz-linux-lts" >> /boot/loader/entries/arch-lts-fallback.conf
 echo "initrd  /initramfs-linux-lts-fallback.img" >> /boot/loader/entries/arch-lts-fallback.conf
-echo "options root=UUID=$f rw" >> /boot/loader/entries/arch-lts-fallback.conf
+echo "options root=UUID=${f} rw" >> /boot/loader/entries/arch-lts-fallback.conf
